f9bcedc35b490cbeed3b06053029157d64cda4cf
co719138 <co719138@ucf.edu>
2024-06-23 21:24:44 -0400

    Adding Create, delete, find/search and update controllers

--- build/api/routes.ts
+++ build/api/routes.ts
@@ -3,7 +3,15 @@
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 import { TsoaRoute, fetchMiddlewares, ExpressTemplateService } from '@tsoa/runtime';
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+import { UpdateCardController } from './../../src/controllers/updateCardController';
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 import { RegisterUserController } from './../../src/controllers/registerController';
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+import { FindCardController } from './../../src/controllers/findCardController';
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+import { DeleteCardController } from './../../src/controllers/deleteCardController';
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+import { CreateCardController } from './../../src/controllers/createCardController';
 import type { Request as ExRequest, Response as ExResponse, RequestHandler, Router } from 'express';
 
 
@@ -11,6 +19,28 @@ import type { Request as ExRequest, Response as ExResponse, RequestHandler, Rout
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
 const models: TsoaRoute.Models = {
+    "UpdateCardErrorResponse": {
+        "dataType": "refObject",
+        "properties": {
+            "message": {"dataType":"string","required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "ICard": {
+        "dataType": "refObject",
+        "properties": {
+            "side_front": {"dataType":"string","required":true},
+            "side_back": {"dataType":"string","required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "UpdateCardParams": {
+        "dataType": "refAlias",
+        "type": {"ref":"ICard","validators":{}},
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     "ErrorResponse": {
         "dataType": "refObject",
         "properties": {
@@ -36,6 +66,45 @@ const models: TsoaRoute.Models = {
         "type": {"ref":"IUser","validators":{}},
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "FindCardErrorResponse": {
+        "dataType": "refObject",
+        "properties": {
+            "message": {"dataType":"string","required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "FindCardParams": {
+        "dataType": "refAlias",
+        "type": {"ref":"ICard","validators":{}},
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "DeleteCardErrorResponse": {
+        "dataType": "refObject",
+        "properties": {
+            "message": {"dataType":"string","required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "DeleteCardParams": {
+        "dataType": "refAlias",
+        "type": {"ref":"ICard","validators":{}},
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "CardErrorResponse": {
+        "dataType": "refObject",
+        "properties": {
+            "message": {"dataType":"string","required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "CreateCardParams": {
+        "dataType": "refAlias",
+        "type": {"ref":"ICard","validators":{}},
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 };
 const templateService = new ExpressTemplateService(models, {"noImplicitAdditionalProperties":"silently-remove-extras","bodyCoercion":true});
 
@@ -46,6 +115,37 @@ export function RegisterRoutes(app: Router) {
     //  NOTE: If you do not see routes for all of your controllers in this file, then you might not have informed tsoa of where to look
     //      Please look into the "controllerPathGlobs" config option described in the readme: https://github.com/lukeautry/tsoa
     // ###########################################################################################################
+        app.post('/api/v1/update',
+            ...(fetchMiddlewares<RequestHandler>(UpdateCardController)),
+            ...(fetchMiddlewares<RequestHandler>(UpdateCardController.prototype.updateCard)),
+
+            async function UpdateCardController_updateCard(request: ExRequest, response: ExResponse, next: any) {
+            const args: Record<string, TsoaRoute.ParameterSchema> = {
+                    body: {"in":"body","name":"body","required":true,"ref":"UpdateCardParams"},
+                    req: {"in":"request","name":"req","required":true,"dataType":"object"},
+            };
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args, request, response });
+
+                const controller = new UpdateCardController();
+
+              await templateService.apiHandler({
+                methodName: 'updateCard',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: undefined,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
         app.post('/api/v1/register',
             ...(fetchMiddlewares<RequestHandler>(RegisterUserController)),
             ...(fetchMiddlewares<RequestHandler>(RegisterUserController.prototype.registerUser)),
@@ -53,6 +153,7 @@ export function RegisterRoutes(app: Router) {
             async function RegisterUserController_registerUser(request: ExRequest, response: ExResponse, next: any) {
             const args: Record<string, TsoaRoute.ParameterSchema> = {
                     body: {"in":"body","name":"body","required":true,"ref":"RegisterUserParams"},
+                    req: {"in":"request","name":"req","required":true,"dataType":"object"},
             };
 
             // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
@@ -65,6 +166,99 @@ export function RegisterRoutes(app: Router) {
 
               await templateService.apiHandler({
                 methodName: 'registerUser',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: undefined,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+        app.post('/api/v1/find',
+            ...(fetchMiddlewares<RequestHandler>(FindCardController)),
+            ...(fetchMiddlewares<RequestHandler>(FindCardController.prototype.findCard)),
+
+            async function FindCardController_findCard(request: ExRequest, response: ExResponse, next: any) {
+            const args: Record<string, TsoaRoute.ParameterSchema> = {
+                    body: {"in":"body","name":"body","required":true,"ref":"FindCardParams"},
+                    req: {"in":"request","name":"req","required":true,"dataType":"object"},
+            };
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args, request, response });
+
+                const controller = new FindCardController();
+
+              await templateService.apiHandler({
+                methodName: 'findCard',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: undefined,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+        app.post('/api/v1/delete',
+            ...(fetchMiddlewares<RequestHandler>(DeleteCardController)),
+            ...(fetchMiddlewares<RequestHandler>(DeleteCardController.prototype.deleteCard)),
+
+            async function DeleteCardController_deleteCard(request: ExRequest, response: ExResponse, next: any) {
+            const args: Record<string, TsoaRoute.ParameterSchema> = {
+                    body: {"in":"body","name":"body","required":true,"ref":"DeleteCardParams"},
+                    req: {"in":"request","name":"req","required":true,"dataType":"object"},
+            };
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args, request, response });
+
+                const controller = new DeleteCardController();
+
+              await templateService.apiHandler({
+                methodName: 'deleteCard',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: undefined,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+        app.post('/api/v1/create',
+            ...(fetchMiddlewares<RequestHandler>(CreateCardController)),
+            ...(fetchMiddlewares<RequestHandler>(CreateCardController.prototype.createCard)),
+
+            async function CreateCardController_createCard(request: ExRequest, response: ExResponse, next: any) {
+            const args: Record<string, TsoaRoute.ParameterSchema> = {
+                    body: {"in":"body","name":"body","required":true,"ref":"CreateCardParams"},
+                    req: {"in":"request","name":"req","required":true,"dataType":"object"},
+            };
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args, request, response });
+
+                const controller = new CreateCardController();
+
+              await templateService.apiHandler({
+                methodName: 'createCard',
                 controller,
                 response,
                 next,
---
+++ src/controllers/createCardController.ts
@@ -0,0 +1,94 @@
+import { ICard} from "glint-core/src/models.js";
+import { GlobalState as GS } from "@state";
+import {
+    Body,
+    Controller,
+    Request,
+    Get,
+    Header,
+    Path,
+    Post,
+    Query,
+    Route,
+    SuccessResponse,
+} from "tsoa";
+import * as bc from "bcrypt";
+import { ObjectId, WithId, Document } from "mongodb";
+import jwt from "jsonwebtoken";
+import * as exp from "express";
+
+const bcryptSaltRounds = 10;
+
+/** 
+* Fields for creating a new card.
+*/
+type CreateCardParams = ICard;
+
+const col = (collection_name: string) => GS.mongo.db.collection(collection_name);
+
+type Doc<T> = (T & WithId<Document>);
+
+interface CardErrorResponse {
+    message: string
+}
+
+function randId(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+const jwtSecret = randId(20);
+
+@Route("/api/v1/create")
+export class CreateCardController extends Controller {
+
+    @Post()
+    public async createCard(@Body() body: CreateCardParams, @Request() req: exp.Request) {
+
+        // Attempt to create a card
+        let createResult = await col("card").insertOne(body);
+
+        // Respond with internal server error if could not insert
+        if (!createResult.acknowledged) {
+            this.setStatus(500);
+            let resp: CardErrorResponse = {
+                message: "database create of card was not acknowledged"
+            };
+            return resp;
+        }
+
+        // Retrieve just now inserted card
+        let cardDoc = await col("card").findOne({ side_front: body.side_back }) as Doc<ICard> | null;
+
+        // Error early if unable to retrieve card
+        if (cardDoc === null) {
+            this.setStatus(500);
+            let resp: CardErrorResponse = {
+                message: "unable to retrieve newly created card"
+            };
+            return resp;
+        }
+
+        // Create JWT payload
+        let authPayload = {
+            sub: cardDoc._id.toString(),
+            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7 * 2)
+        };
+
+        // Sign payload
+        let signedJwt = jwt.sign(authPayload, jwtSecret);
+
+        // Set cookie header to contain JWT authentication payload
+        this.setHeader("Set-Cookie", `auth=${signedJwt}`);
+
+        return;
+    }
+
+}
